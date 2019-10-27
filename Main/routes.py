71d3a5ecace18fcaba5d261d4399b695d1b206e3
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2019-10-27 15:17:19 +0530

    Add files via upload

--- Main/routes.py
+++ Main/routes.py
@@ -9,6 +9,11 @@ def index():
     if request.method == 'POST':
         Season = request.form['Season']
         Series = request.form['Series']
-        print('########################',Season, Series)
+        Series = str(Series)
+        Series.replace(" ", "_")
+        url = 'https://en.wikipedia.org/wiki/'+Series+'_(season_'+Season+')'
+        print('This is URL:',url)
+        #insert beautiful soup work here
+        #Return Statements will be changed
         return jsonify({'Season':Season, 'Series': Series})
     return jsonify({'Error':'Missing Data' })
