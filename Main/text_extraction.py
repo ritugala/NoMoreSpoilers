fc9f59747d7aacf1b65c60f727ab49605fdf94ac
revathivijay <34963337+revathivijay@users.noreply.github.com>
2019-10-30 10:19:28 +0530

    Add files via upload

--- Main/text_extraction.py
+++ Main/text_extraction.py
@@ -104,37 +104,49 @@ def sort_coo(coo_matrix):
     tuples = zip(coo_matrix.col, coo_matrix.data)
     return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
 
-def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+def extract_topn_from_vector(feature_names, sorted_items, topn=10, flag=0):
     """get the feature names and tf-idf score of top n items"""
 
-    #use only topn items from vector
-    sorted_items = sorted_items[:topn]
+    if flag==1:
+        #use only topn items from vector
+        sorted_items = sorted_items[:topn]
 
-    score_vals = []
-    feature_vals = []
+        score_vals = []
+        feature_vals = []
 
-    # word index and corresponding tf-idf score
-    for idx, score in sorted_items:
+        # word index and corresponding tf-idf score
+        for idx, score in sorted_items:
+            #keep track of feature name and its corresponding score
+            score_vals.append(round(score, 3))
+            feature_vals.append(feature_names[idx])
 
-        #keep track of feature name and its corresponding score
-        score_vals.append(round(score, 3))
-        feature_vals.append(feature_names[idx])
+        #create a dictionary of feature,score
+        results= {}
+        for idx in range(len(feature_vals)):
+            results[feature_vals[idx]]=score_vals[idx]
+        return results
+    else:
+        score_vals = []
+        feature_vals = []
+        # word index and corresponding tf-idf score
+        for idx, score in sorted_items:
+            #keep track of feature name and its corresponding score
+            score_vals.append(round(score, 3))
+            feature_vals.append(feature_names[idx])
 
-    #create a tuples of feature,score
-    #results = zip(feature_vals,score_vals)
-    results= {}
-    for idx in range(len(feature_vals)):
-        results[feature_vals[idx]]=score_vals[idx]
+        #create a dictionary of feature,score
+        results= {}
+        for idx in range(len(feature_vals)):
+            results[feature_vals[idx]]=score_vals[idx]
+        return results
 
-    return results
+
 #sort the tf-idf vectors by descending order of scores
 sorted_items=sort_coo(tf_idf_vector.tocoo())
-#extract only the top n
-keywords=extract_topn_from_vector(feature_names,sorted_items,20)
 
-# # now print the results
-# print("\nAbstract:")
-# print(doc)
+#extract only the top n and set flag to 1, default flag is 0---will pick ALL possible keywords
+keywords=extract_topn_from_vector(feature_names,sorted_items)
+
 print("\nKeywords:")
 for k in keywords:
     print(k,keywords[k])
