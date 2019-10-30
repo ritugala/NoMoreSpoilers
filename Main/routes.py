bac294a3eaa35cf2a09d502c0a1111b505609774
revathivijay <34963337+revathivijay@users.noreply.github.com>
2019-10-30 10:47:58 +0530

    Structuring the project

--- Main/routes.py
+++ Main/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, jsonify
 from Main import app
+from web_scraper import WebScraper
+from text_extraction import TextExtractor
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -13,6 +15,16 @@ def index():
         Series = str(Series)
         Series.replace(" ", "_")
         url = 'https://en.wikipedia.org/wiki/'+Series+'_(season_'+Season+')'
-        print('This is URL:',url)
-        return jsonify({'keywords':Series})
+        print('This is URL:', url)
+
+        #web scraping
+        scraper = WebScraper()
+        text = scraper.web_scraper(url)
+
+        #text mining
+        miner = TextExtractor()
+        keywords = miner.text_extractor(text)
+
+        #Return Statements will be changed
+        return jsonify({'keywords':keywords})
     return jsonify({'Error':'Missing Data' })
